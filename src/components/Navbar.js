5be3afe5bc52a051a5f07133302b14dbb42910a5
Jorge Mayoral Álvarez <mayoralalvarezj@gmail.com>
2021-07-24 11:02:06 +0200

    perf: :lipstick: Update navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,41 +1,71 @@
+import { CloseIcon, HamburgerIcon } from '@chakra-ui/icons';
 import { Box, Center, Flex, Heading, Link, Stack } from '@chakra-ui/layout';
+import { HStack, IconButton, useDisclosure } from '@chakra-ui/react';
 import NextLink from 'next/link';
 
+const Links = ['Inicio', 'Blog', 'Contacto'];
+
+const NavLink = ({ children }) => (
+  <NextLink href={children === 'Inicio' ? '/' : `/${children.toLowerCase()}`}>
+    <Link
+      px={2}
+      py={1}
+      rounded={'md'}
+      _hover={{ textDecoration: 'none', bg: 'gray.200' }}
+    >
+      {children}
+    </Link>
+  </NextLink>
+);
+
 const Navbar = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <Flex zIndex={1} position="sticky" top={0} bg="#fff" p={4}>
-      <Flex flex={1} m="auto" align="center" maxW={800}>
-        <NextLink href="/">
-          <Link _hover={{ textDecoration: 'none' }}>
-            <Center>
-              <Heading color="#00adb5" size="lg">
-                Dev
-              </Heading>
-              <Heading size="lg">Yorch</Heading>
-            </Center>
-          </Link>
-        </NextLink>
-        <Box ml="auto">
-          <Stack direction="row">
-            <NextLink href="/">
-              <Link mx={2}>
-                <a>Inicio</a>
-              </Link>
-            </NextLink>
-            <NextLink href="/blog">
-              <Link mx={2}>
-                <a>Blog</a>
-              </Link>
-            </NextLink>
-            <NextLink href="/contacto">
-              <Link mx={2}>
-                <a>Contacto</a>
-              </Link>
-            </NextLink>
-          </Stack>
-        </Box>
-      </Flex>
-    </Flex>
+    <>
+      <Box bg="#fff" px={4}>
+        <Flex h={16} alignItems={'center'} justifyContent={'space-between'}>
+          <IconButton
+            size={'md'}
+            icon={isOpen ? <CloseIcon /> : <HamburgerIcon />}
+            aria-label={'Abrir Menu'}
+            display={{ md: 'none' }}
+            onClick={isOpen ? onClose : onOpen}
+          />
+          <HStack spacing={8} alignItems={'center'}>
+            <Box _hover={{ cursor: 'pointer' }}>
+              <NextLink href="/">
+                <Center>
+                  <Heading color="#00adb5" size="lg">
+                    Dev
+                  </Heading>
+                  <Heading size="lg">Yorch</Heading>
+                </Center>
+              </NextLink>
+            </Box>
+            <HStack
+              as={'nav'}
+              spacing={4}
+              display={{ base: 'none', md: 'flex' }}
+            >
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </HStack>
+          </HStack>
+        </Flex>
+
+        {isOpen ? (
+          <Box pb={4} display={{ md: 'none' }}>
+            <Stack as={'nav'} spacing={4}>
+              {Links.map((link) => (
+                <NavLink key={link}>{link}</NavLink>
+              ))}
+            </Stack>
+          </Box>
+        ) : null}
+      </Box>
+    </>
   );
 };
 
